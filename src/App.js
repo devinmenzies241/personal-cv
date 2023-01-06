5630af3a53f8af08605bd0dd73942678c0da9349
Womp Rat <devinmenzies@gmail.com>
2023-01-06 08:54:44 -0700

    Initial commit Personal CV

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/menu.jsx'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-home">
+        <div className="header">
+          <Menu />
+          <h1>Devin Menzies</h1>
+          <h2>Front End Web Developer</h2>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/menu.jsx
@@ -0,0 +1,13 @@
+const Menu = () => {
+  return (
+    <div class="vertical-menu">
+      <a href="#" class="active">Home</a>
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+      <a href="#">Link 4</a>
+    </div>
+  )
+}
+
+export default Menu; 
